858f63e0ae5cd2ad1728945f42df25a1e5a2fccd
Vinayak Dubey <88390255+VinayakDubey07@users.noreply.github.com>
2022-12-05 23:10:27 +0530

    added string and multiset instead of int as input

--- splitwise.cpp
+++ splitwise.cpp
@@ -1,49 +1,73 @@
 #include <iostream>
 #include<set>
+#include<map>
 using namespace std;
+
+class person_compare{
+    public:
+    bool operator()(pair<string,int> p1, pair<string,int> p2){
+        return p1.second < p2.second;
+    }
+};
+
+
+
 int main() {
     
 int no_of_transactions,friends;
 cin>>no_of_transactions>>friends;
 
 
+string x,y;
+int amount;
 
-int x,y,amount;
-
-int net[100000] = {0};
+map<string,int> net;
 while(no_of_transactions--){
     cin>>x>>y;
+    if(net.count(x)==0){
+        net[x] = 0;
+    }
+    if(net.count(y)==0){
+net[y] = 0;
+    }
     net[x] -= amount;
     net[y] += amount;
 }
-multiset<int> m;
-for(int i=0;i<friends;i++){
-    if(net[i]!=0){
-        m.insert(net[i]);
+multiset<pair<string, int>, person_compare>m;
+for(auto p:net){
+    string person = p.first;
+    int amount = p.second;
+    if(net[person]!=0){
+        m.insert(make_pair(person,amount));
     }
 }
+
 int cnt = 0;
 while(!m.empty()){
     auto low = m.begin();
     auto high = prev(m.end());
 
-    int debit = *low;
-    int credit = *high;
+    int debit = low->second;
+    string debit_person = low->first;
 
-m.erase(low);
-m.erase(high);
+    int credit = high->second;
+    string credit_person = high->first;
 
-int settlement_amount = min(-debit,credit);
-cnt++;
-debit += settlement_amount;
-credit -= settlement_amount;
-if(debit!=0){
-    m.insert(debit);
-}
-if(credit!=0){
-    m.insert(credit);
-}
+    m.erase(low);
+    m.erase(high);
+    int settled_amount = min(-debit,credit);
+    debit += settled_amount;
+    credit -= settled_amount;
 
+    cout<<debit_person<<"will pay"<<settled_amount<<"to"<<credit_person<<endl;
+
+    if(debit!=0){
+        m.insert(make_pair(debit_person,debit));
+    }
+    if(credit!=0){
+        m.insert(make_pair(credit_person,credit));
+    }
+    cnt += 1;
 }
 cout<<cnt<<endl;
     return 0;
